fix computing rotation matrix

eye_detector/capture_dlib/computes.py
@@ -1,7 +1,5 @@
 from typing import Optional
-from time import time
 
-import cv2
 import numpy as np
 from scipy.spatial.transform import Rotation
 
@@ -39,7 +37,6 @@ def update_pointer_coords(screen_box: ScreenBox, eyecache: EyeCache, eye_3d: Opt
 
 
 def compute_face_normal(landmarks, cap, depth_frame):
-    #return np.array([0.0, 0.0, -1.0])
     points = [
         cap.to_3d(np.array([p.x, p.y]), depth_frame) for p in [
             landmarks.part(8),  # Chin
@@ -54,29 +51,35 @@ def compute_face_normal(landmarks, cap, depth_frame):
     return np.cross(points[1] - points[0], points[2] - points[1])
 
 
-def compute_rotation_matrix(landmarks, cap, depth_frame) -> Rotation:
-    model_points = np.array([
-        [0.0, 0.0, 0.0],
-        [0.0, -330.0, -65.0],
-        [-225.0, 170.0, -135.0],
-        [225.0, 170.0, -135.0],
-        [-150.0, -150.0, -125.0],
-        [150.0, -150.0, -125.0],
-    ], dtype=float)
-
-    points = np.array([
-        [p.x, p.y] for p in [
-            landmarks.part(30),
-            landmarks.part(8),
-            landmarks.part(36),
-            landmarks.part(45),
-            landmarks.part(48),
-            landmarks.part(54),
+def compute_rotation_matrix1(landmarks, cap, depth_frame) -> Rotation:
+    points = [
+        cap.to_3d(np.array([p.x, p.y]), depth_frame) for p in [
+            landmarks.part(8),  # Chin
+            landmarks.part(45),  # Right eye right corner
+            landmarks.part(36),  # Left eye left corner
         ]
+    ]
+
+    if any(p is None for p in points):
+        return None
+
+    eye_len_half = np.linalg.norm(points[1] - points[2]) / 2.0
+    side_a_len = np.linalg.norm(points[0] - points[1])
+    side_b_len = np.linalg.norm(points[0] - points[2])
+    sides_len = (side_a_len + side_b_len) / 2.0
+    face_height = np.sqrt(sides_len ** 2 - eye_len_half ** 2)
+
+    points_to_rotate = np.array([
+        [0, face_height, 0],  # chin
+        [+eye_len_half, 0, 0],  # Right eye corner
+        [-eye_len_half, 0, 0],  # Left eye corner
     ], dtype=float)
 
-    _, rotvec, _ = cv2.solvePnP(model_points, points, cap.camera_matrix, np.zeros((4, 1), dtype=float))
-    return Rotation.from_rotvec(rotvec[..., 0])
+    points = np.array(points)
+    points -= np.sum(points, axis=0) / len(points)
+    points_to_rotate -= np.sum(points_to_rotate, axis=0) / len(points_to_rotate)
+    rotation, _ = Rotation.align_vectors(points, points_to_rotate)
+    return rotation
 
 def compute_rotation_matrix2(landmarks, cap, depth_frame) -> Rotation:
     points = [
@@ -91,12 +94,6 @@ def compute_rotation_matrix2(landmarks, cap, depth_frame) -> Rotation:
     if any(p is None for p in points):
         return None
 
-    eye_len_half = np.linalg.norm(points[1] - points[2]) / 2.0
-    side_a_len = np.linalg.norm(points[0] - points[1])
-    side_b_len = np.linalg.norm(points[0] - points[2])
-    sides_len = (side_a_len + side_b_len) / 2.0
-    face_height = np.sqrt(sides_len ** 2 - eye_len_half ** 2)
-
     chin_len_half = np.linalg.norm(points[0] - points[1]) / 2.0
     eye_len_half = np.linalg.norm(points[2] - points[3]) / 2.0
     a_height = np.linalg.norm(points[0] - points[2])
@@ -106,23 +103,18 @@ def compute_rotation_matrix2(landmarks, cap, depth_frame) -> Rotation:
     points_to_rotate = np.array([
         [-chin_len_half, 0, 0],  # Left chin
         [+chin_len_half, 0, 0],  # Right chin
-        [-eye_len_half, height, 0],  # Left eye corner
-        [+eye_len_half, height, 0],  # Right eye corner
+        [-eye_len_half, -height, 0],  # Left eye corner
+        [+eye_len_half, -height, 0],  # Right eye corner
     ], dtype=float)
 
     points = np.array(points)
     points -= np.sum(points, axis=0) / len(points)
     points_to_rotate -= np.sum(points_to_rotate, axis=0) / len(points_to_rotate)
-    print(points)
-    print(points_to_rotate)
-    rotation, _ = Rotation.align_vectors(points_to_rotate, points)
+    rotation, _ = Rotation.align_vectors(points, points_to_rotate)
     return rotation
 
 
 def compute_rotation_matrix3(landmarks, cap, depth_frame) -> Optional[Rotation]:
-    #face = compute_face_normal(landmarks, cap, depth_frame)
-    #return Rotation.from_rotvec(face) if face is not None else None
-
     points = [
         cap.to_3d(np.array([p.x, p.y]), depth_frame) for p in [
             landmarks.part(30),
@@ -146,5 +138,8 @@ def compute_rotation_matrix3(landmarks, cap, depth_frame) -> Optional[Rotation]:
         [150.0, -150.0, -125.0],
     ], dtype=float)
 
-    rotation, _ = Rotation.align_vectors(points_to_rotate, points)
-    return rotation
+    points = np.array(points)
+    points -= np.sum(points, axis=0) / len(points)
+    points_to_rotate -= np.sum(points_to_rotate, axis=0) / len(points_to_rotate)
+    rotation, _ = Rotation.align_vectors(points, points_to_rotate)
+    return rotation * Rotation.from_euler('x', 180, degrees=True)
a0bf28de0e4161112e728532c0b9d795e6f04083
Firemark <marpiechula@gmail.com>
2025-04-12 15:44:01 +0200

    Change loss function for gaze training

--- src/eye_detector/train_gaze/train.py
+++ src/eye_detector/train_gaze/train.py
@@ -1,17 +1,27 @@
 from time import monotonic
 
+import torch
 from torch.nn import MSELoss
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 
 LOG_FORMAT = '[%2d, %7d / %7d] loss: %.5f; time: %.3f'
+EPS = 1e-3
+
+
+def angle_loss(output, target):
+    output_norm = torch.norm(output, dim=1)
+    target_norm = torch.norm(target, dim=1)
+    dot = (output * target).sum(axis=1)
+    return (dot / (output_norm * target_norm)).clamp(-1+EPS, +1-EPS).acos().sum()
 
 
 def train(dataset_train, dataset_test, test, net, max_epoch=10):
     trainloader = DataLoader(dataset_train, batch_size=20, shuffle=True, num_workers=4)
     data_size = len(trainloader)
     criterion = MSELoss()
+    criterion = angle_loss
     optimizer = Adam(net.parameters(), lr=0.001)
 
     for epoch in range(max_epoch):  # loop over the dataset multiple times
@@ -36,7 +46,7 @@ def train(dataset_train, dataset_test, test, net, max_epoch=10):
             running_loss += loss.item()
             epoch_running_loss += loss.item()
             total += len(gazes)
-            if i % 500 == 0:
+            if i != 0 and i % 200 == 0:
                 print(LOG_FORMAT % (epoch + 1, total, data_size, running_loss / 500, monotonic() - time))
                 running_loss = 0.0
                 time = monotonic()
